aca611836e2c1acfc4b245aed2c9cf3cab95680a
MagoNatali <62276956+MagoNatali@users.noreply.github.com>
2020-03-17 12:21:16 -0500

    Add files via upload

---
+++ Propuesto01.cpp
@@ -0,0 +1,37 @@
+#include <iostream>
+
+using namespace std;
+
+int main(){
+	int numeroAsociado;
+	cout<<"LISTADO DE PLANETAS"<<endl;
+	cout<<"1. Mercurio"<<endl;
+	cout<<"2. Venus"<<endl;
+	cout<<"3. Tierra"<<endl;
+	cout<<"4. Marte"<<endl;
+	cout<<"5. Jupiter"<<endl;
+	cout<<"6. Saturno"<<endl;
+	cout<<"7. Urano"<<endl;
+	cout<<"8. Neptuno"<<endl;
+	cout<<"Ingrese un Numero asociado para saber la distancia media al SOL"<<endl;
+	cin>>numeroAsociado;
+	if(numeroAsociado == 1)
+		cout<<"Mercurio: 59 Millones de Kilometros"<<endl;
+	else if(numeroAsociado == 2)
+		cout<<"Venus:108 Millones de Kilometros"<<endl;
+	else if(numeroAsociado == 3)
+		cout<<"Tierra:150 Millones de Kilometros"<<endl;
+	else if(numeroAsociado == 4)
+		cout<<"Marte:228 Millones de Kilometros"<<endl;
+	else if(numeroAsociado == 5)
+		cout<<"Jupiter:750 Millones de Kilometros"<<endl;
+	else if(numeroAsociado == 6)
+		cout<<"Saturno:1431 Millones de Kilometros"<<endl;
+	else if(numeroAsociado == 7)
+		cout<<"Urano:2877 Millones de Kilometros"<<endl;
+	else if(numeroAsociado == 8)
+		cout<<"Neptuno:4509 Millones de Kilometros"<<endl;
+	else
+		cout<<"ERROR: "<<numeroAsociado<<" no está asociado a ningún planeta."<<endl;
+	return 0;
+}
---
+++ Propuesto02.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+
+using namespace std;
+
+int main(){
+	int numeroAsociado;
+	cout<<"Colores de un Semaforo"<<endl;
+	cout<<"1. ROJO"<<endl;
+	cout<<"2. VERDE"<<endl;
+	cout<<"3. AMARILLO"<<endl;
+	cout<<"Ingrese un Numero Asociado"<<endl;
+	cin>>numeroAsociado;
+	if(numeroAsociado==1)
+		cout<<"Color ROJO: NO PASAR"<<endl;
+	else if(numeroAsociado==2)
+		cout<<"Color VERDE: ADELANTE"<<endl;
+	else if(numeroAsociado==3)
+		cout<<"Color AMARILLO: PRECAUCION"<<endl;
+	else
+		cout<<"ERROR NUMERO ASOCIADO NO VALIDO"<<endl;
+	return 0;
+}
